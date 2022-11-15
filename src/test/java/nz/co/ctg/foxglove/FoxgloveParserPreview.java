3b8e5f9227a28a58e7d59730d0c0516838e6c772
Colin Sharples <ctg@ctg.co.nz>
2022-11-16 09:39:14 +1300

    Add selector for images in parser preview

--- src/test/java/nz/co/ctg/foxglove/FoxgloveParserPreview.java
+++ src/test/java/nz/co/ctg/foxglove/FoxgloveParserPreview.java
@@ -1,10 +1,19 @@
 package nz.co.ctg.foxglove;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
@@ -20,6 +29,8 @@ public class FoxgloveParserPreview extends Application {
     }
 
     private Stage mainStage;
+    private ScrollPane scrollPane;
+    private Path defaultFile;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -27,7 +38,10 @@ public class FoxgloveParserPreview extends Application {
         BorderPane root = new BorderPane();
         root.setPadding(new Insets(12));
 
-        root.setCenter(new ScrollPane(createGraphic()));
+        defaultFile = Paths.get(SvgGraphic.class.getResource("/test.svg").toURI());
+        root.setTop(createToolbar());
+        scrollPane = new ScrollPane(createGraphic(defaultFile));
+        root.setCenter(scrollPane);
 
         Scene scene = new Scene(root);
         mainStage.setScene(scene);
@@ -37,9 +51,32 @@ public class FoxgloveParserPreview extends Application {
         mainStage.show();
     }
 
-    private Node createGraphic() throws Exception {
+    private Node createToolbar() {
+        ComboBox<Path> files = new ComboBox<>(findSvgFiles());
+        files.getSelectionModel().select(defaultFile);
+        files.setOnAction(evt -> {
+            Path filePath = files.getSelectionModel().getSelectedItem();
+            try {
+                scrollPane.setContent(createGraphic(filePath));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        return new ToolBar(files);
+    }
+
+    private ObservableList<Path> findSvgFiles() {
+        try {
+            return FXCollections.observableArrayList(Files.list(defaultFile.getParent()).filter(path -> path.toString().endsWith(".svg")).collect(Collectors.toList()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return FXCollections.emptyObservableList();
+        }
+    }
+
+    private Node createGraphic(Path filePath) throws Exception {
         FoxgloveParser parser = new FoxgloveParser();
-        SvgGraphic svgElement = parser.parse(SvgGraphic.class.getResourceAsStream("/test.svg"));
+        SvgGraphic svgElement = parser.parse(Files.newInputStream(filePath));
         Region graphic = svgElement.createGraphic();
         graphic.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.DOTTED, null, BorderStroke.THIN)));
         return graphic;
